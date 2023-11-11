de1abc1a9c1a10056bdcaf4cc35d79d125a6a44a
Panji <backend@lexigo.com>
2023-11-11 12:36:31 +0700

    fix

--- dist/assets/AboutView-020619a9.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as s,a as o}from"./index-651bf930.js";const _={},c={class:"about"},n=o("h1",null,"This is website for Skill Test",-1),a=[n];function i(r,l){return t(),s("div",c,a)}const u=e(_,[["render",i]]);export{u as default};
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory("/vue-test/"),
+  history: createWebHistory("/vue-test/dist/"),
   routes: [
     {
       path: '/',
